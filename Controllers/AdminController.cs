5f8cdba984fa5f9d2f00a5b27d4081bc06e64c91
JBellows-git <jbellows212@gmail.com>
2022-05-18 17:23:29 -0500

    can now assign Roles to Users

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AdventureProject.Data;
+using AdventureProject.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -32,11 +33,35 @@ namespace AdventureProject.Controllers
             return View();
         }
 
-        public IActionResult AllUsers()
+        public async Task<IActionResult> AllUsers()
         {
-            ViewBag.Users = _userManager.Users.ToList();
-            ViewBag.Players = _context.PlayerCharacters.ToList();
-            return View();
+            //rework view to use new model instead of wonky dictionary that we removed
+            List<Models.ViewModels.UserView> userViews = new List<Models.ViewModels.UserView>();            
+            var users = _userManager.Users.ToList();
+            var players = _context.PlayerCharacters.ToList();
+            var roles = _roleManager.Roles.ToList();
+            foreach (var u in users)
+            {
+                Models.ViewModels.UserView temp = new Models.ViewModels.UserView();
+                foreach(var p in players)
+                {
+                    if(p.UserID == u.Id)
+                    {
+                        temp.Username = u.UserName;
+                        temp.CharacterName = p.CharacterName;
+                        var roleList = await _userManager.GetRolesAsync(u);
+                        var roleTemp = String.Join(", ", roleList);
+                        temp.Roles = roleTemp;
+                        userViews.Add(temp);
+                    }
+                    
+                    
+                }
+            }
+            ViewBag.Users = users;
+            ViewBag.Roles = roles;
+            
+            return View(userViews);
         }
 
         [HttpPost]
@@ -46,13 +71,26 @@ namespace AdventureProject.Controllers
             try
             {
                 await _roleManager.CreateAsync(new IdentityRole(name));
-                ViewBag.message = "Creation Successful";
+                ViewBag.message = "Creation Successful";                
                 
             } catch {
                 ViewBag.message = "Creation Failed";
             }
             return View();
         }
+
+        [HttpPost]
+        public async Task<ActionResult> AssignRole(string role, string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            try
+            {
+                await _userManager.AddToRoleAsync(user, role);
+            }
+            catch { }
+            return RedirectToAction("AllUsers");
+        }
+
     }
 
     
---
+++ Models/ViewModels/UserView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdventureProject.Models.ViewModels
+{
+    public class UserView
+    {
+        public string Username { get; set; }
+        public string Roles { get; set; }
+        public string CharacterName { get; set; }
+
+        public UserView()
+        {
+        }
+
+        public UserView(string username, string roles, string characterName)
+        {
+            Username = username;
+            Roles = roles;
+            CharacterName = characterName;
+        }
+    }
+}
